7e5df860d40d81ae08ec13c6e8a8a4def730e7d9
Bui Dai <ss1280dzzz@gmail.com>
2025-09-27 10:42:14 +0700

    feat(model): basic model as baseline

---
+++ src/model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class PaddyResNet(nn.Module):
+    def __init__(self, num_classes: int):
+        super().__init__()
+
+        self.base_model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+
+        for param in self.base_model.parameters():
+            param.requires_grad = False
+
+        in_features = self.base_model.fc.in_features
+        self.base_model.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.base_model(x)
